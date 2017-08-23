7a5e170dd9e660e17ce6caa36a9b8499366b6a49
Zhi-Wei Yang <allen1759@gmail.com>
2017-08-23 20:02:34 +0800

    Update TransitionalVideo module.

--- SmilingBuddha/SmilingBuddha/TransitionalVideo.h
+++ SmilingBuddha/SmilingBuddha/TransitionalVideo.h
@@ -9,39 +9,51 @@
 
 #include "Video.h"
 
+#include <chrono>
+
 #include <iostream>
 
 class TransitionalVideo : public Video
 {
 public:
-	TransitionalVideo(Video * video, Video * nextVideo)
+	TransitionalVideo(std::shared_ptr<Video> video, std::shared_ptr<Video> nextVideo, float duration)
 	{
 		this->video = video->GetVideo();
 		this->nextVideo = nextVideo->GetVideo();
+
+		this->duration = std::chrono::milliseconds(static_cast<int>(duration * 1000));
+		startTime = std::chrono::high_resolution_clock::now();
 	}
 	~TransitionalVideo()
 	{}
 
-	virtual std::shared_ptr<cv::Mat> GetFrame() override;
+	virtual std::shared_ptr<cv::Mat> GetFrame() = 0;
 
-	virtual Video * GetVideo() override
-	{
-		return nextVideo;
-	}
+	virtual std::shared_ptr<Video> GetVideo() override;
 
 protected:
 
-	Video * video;
-	Video * nextVideo;
+	std::shared_ptr<Video> video;
+	std::shared_ptr<Video> nextVideo;
+
+	std::chrono::milliseconds duration;
+
+	std::chrono::high_resolution_clock::time_point startTime;
 };
 
 
-std::shared_ptr<cv::Mat> TransitionalVideo::GetFrame()
+//std::shared_ptr<cv::Mat> TransitionalVideo::GetFrame()
+//{
+//	std::cout << "Transitional Video" << std::endl;
+//
+//	video->GetFrame();
+//	nextVideo->GetFrame();
+//	return NULL;
+//}
+
+std::shared_ptr<Video> TransitionalVideo::GetVideo()
 {
-	video->GetFrame();
-	nextVideo->GetFrame();
-	std::cout << "Transitional Video" << std::endl;
-	return NULL;
+	return nextVideo;
 }
 
 #endif // !_TRANSITIONAL_VIDEO_H
