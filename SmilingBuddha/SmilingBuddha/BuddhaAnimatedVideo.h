5f20513e14dfa5f6189f976ffa6f8452c90043c9
Yu-Ci, Huang <j810326@gmail.com>
2017-09-16 13:34:53 +0800

    Add BuddhaAnimatedVideo.

---
+++ SmilingBuddha/SmilingBuddha/BuddhaAnimatedVideo.h
@@ -0,0 +1,44 @@
+/**
+* Created by Yu-Ci Huang <j810326@gmail.com>.
+* Copyright(c) 2015-2017 imLab. All rights reserved.
+*
+*/
+
+#ifndef _BUDDHA_ANIMATED_VIDEO_H
+#define _BUDDHA_ANIMATED_VIDEO_H
+
+#include "AnimatedVideo.h"
+
+class BuddhaAnimatedVideo : public AnimatedVideo
+{
+public:
+	BuddhaAnimatedVideo(std::shared_ptr<Video> video, float duration, int centerX, int centerY);
+
+	virtual ~BuddhaAnimatedVideo() override;
+
+	virtual std::shared_ptr<cv::Mat> GetFrame() override;
+
+private:
+	//TODO: make constant.
+	float PERIOD = 3.14159f * 80.0f;
+	
+	float WAVE_AMPLITUDE = 0.025f;
+
+	float radius;
+	float buddhaRadius;
+
+	std::shared_ptr<cv::Mat> blendFrame;
+	std::shared_ptr<cv::Mat> currentFrame;
+	std::shared_ptr<cv::Mat> buddhaFrame;
+
+	int centerX;
+	int centerY;
+
+	std::chrono::high_resolution_clock::time_point lastTime;
+	float secondsPerFrame;
+
+	bool end;
+
+};
+
+#endif // !_BUDDHA_ANIMATED_VIDEO_H
